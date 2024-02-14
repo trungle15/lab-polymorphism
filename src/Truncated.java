e7e78e3c47ea5abfe6d08016aac9ef811dc35b88
trungle15 <50526463+trungle15@users.noreply.github.com>
2024-02-13 19:33:00 -0600

    Add truncated class

---
+++ src/Truncated.java
@@ -0,0 +1,34 @@
+public class Truncated implements TextBlock{
+    
+    // Fields
+    TextBlock content;
+    int width;
+
+    // Constructors
+
+    public Truncated (TextBlock content, int width) throws Exception {
+        this.content = content;
+        if (width < 0) {
+            throw new Exception("Invalid max width");
+        } else {
+            this.width = width;
+        }
+    }
+
+    public String row(int i) throws Exception {
+        int contentWidth = this.content.width();
+        if (contentWidth > width) {
+            return content.row(i).substring(0, width);
+        } else {
+        return content.row(i);}
+    }
+    
+    public int height() {
+        return content.height();
+    }
+
+    public int width() {
+        return content.width();
+    }
+    
+}
